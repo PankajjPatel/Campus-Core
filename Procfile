c8187532879862ad1d2329e117a1a4033a9af07b
Pankaj Patel <pankajlucky678@gmail.com>
2026-08-14 16:22:29 +0530

    Add render.yaml, Procfile, and PyMySQL version patch

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn CampusCore.wsgi:application --bind 0.0.0.0:${PORT:-8000} --log-file -
--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 # exit on error
 set -o errexit
 
-python manage.py migrate
+python manage.py migrate --no-input
 python manage.py seed_data || true
-gunicorn CampusCore.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn CampusCore.wsgi:application --bind 0.0.0.0:${PORT:-8000} --log-file -
