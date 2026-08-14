499211cc1eaf6160ffa8fbbf6d3ca27a36f39fbf
Pankaj Patel <pankajlucky678@gmail.com>
2026-08-14 16:35:28 +0530

    Use unconstrained django package in requirements.txt and inline render commands

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
-
 python manage.py migrate --no-input
-python manage.py seed_data || true
-gunicorn CampusCore.wsgi:application --bind 0.0.0.0:${PORT:-8000} --log-file -
+gunicorn CampusCore.wsgi:application --bind 0.0.0.0:$PORT
